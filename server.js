3de23e41b036c6071b01af3ad30c5e4c299f4605
Wilson <wilsonhorrell@git.training>
2021-01-12 10:50:10 -0600

    Again

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ var MongoStore = require("connect-mongo")(session);
 const cookieParser = require("cookie-parser");
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 

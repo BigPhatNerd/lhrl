0e42380c29ea5d2c13ecdf1591ad3207b87ff11a
Wilson <wilsonhorrell@git.training>
2021-03-23 13:28:11 -0500

    addressed all the emojis

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
              <p className="lead">
              <p>Install LHRL® App to your Slack workspace</p>
              <br />
-             <p style={{margin: "1rem"}}>We encourage your to create a channel named <strong>#lhrl</strong> in your Slack workspace for the app to post.</p>
+             <p style={{margin: "1rem"}}>Before installing, we encourage your to create a channel named <strong>#lhrl</strong> in your Slack workspace for the app to post.</p>
              <br />
        <a href="https://slack.com/oauth/v2/authorize?client_id=1093878125127.1152997432560&scope=commands,incoming-webhook,users.profile:read,users:read,chat:write&user_scope=chat:write,users.profile:read,users:read,users:read.email"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
         <br />

2769f8308f99a77e1365f4a9af840ff7acafdd51
BigPhatNerd <wilsonhorrell@gmail.com>
2021-08-13 09:25:41 -0500

    Added date to Strava

--- slack-templates/strava-templates.js
+++ slack-templates/strava-templates.js
@@ -1,72 +1,106 @@
+var dayjs = require("dayjs");
 const {
-    activityType,
-    getMiles,
-    getKilometers,
-    timeOfWorkout,
-    avgMile
-} = require('../utils/strava');
+  activityType,
+  getMiles,
+  getKilometers,
+  timeOfWorkout,
+  avgMile,
+} = require("../utils/strava");
 const slackTemplates = {
-    stravaHook(response, username, avatar, channel) {
-        // response = JSON.stringify(response);
-        const {
-            athlete,
-            type,
-            distance,
-            elapsed_time,
-            moving_time,
-            average_speed,
-            max_speed,
-            map
-        } = response;
-        array = [];
+  stravaHook(response, username, avatar, channel) {
+    // response = JSON.stringify(response);
+    const {
+      athlete,
+      type,
+      distance,
+      elapsed_time,
+      moving_time,
+      average_speed,
+      max_speed,
+      map,
+      start_date,
+    } = response;
+    array = [];
 
-        const blockData = (username, distance, elapsed_time, moving_time, average_speed, max_speed) => {
-            array.push({
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Athlete: " + username + "\n" +
-                        "Type of Exercise: " + activityType(type) + "\n" +
-                        "Distance: " + getMiles(distance) + "miles / " + getKilometers(distance) + "km's\n" +
-                        "Time: " + timeOfWorkout(elapsed_time) + "\n" +
-                        "Average Speed: " + avgMile(elapsed_time, distance) + "\n"
-
-
-                },
-                "accessory": {
-                    "type": "image",
-                    "image_url": avatar,
-                    "alt_text": "alt text for image"
-                }
-            }, {
-                "type": "divider"
-            });
-            return array
+    const blockData = (
+      start_date,
+      username,
+      distance,
+      elapsed_time,
+      moving_time,
+      average_speed,
+      max_speed
+    ) => {
+      array.push(
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text:
+              "Athlete: " +
+              username +
+              "\n" +
+              "Date: " +
+              dayjs(start_date).format("dddd MMMM D YYYY") +
+              "\n" +
+              "Type of Exercise: " +
+              activityType(type) +
+              "\n" +
+              "Distance: " +
+              getMiles(distance) +
+              "miles / " +
+              getKilometers(distance) +
+              "km's\n" +
+              "Time: " +
+              timeOfWorkout(elapsed_time) +
+              "\n" +
+              "Average Speed: " +
+              avgMile(elapsed_time, distance) +
+              "\n",
+          },
+          accessory: {
+            type: "image",
+            image_url: avatar,
+            alt_text: "alt text for image",
+          },
+        },
+        {
+          type: "divider",
         }
+      );
+      return array;
+    };
 
-        const stravaReturn = {
-"channel": channel,
-            "text": username + " just entered a workout on Strava. ",
-            "blocks": blockData(username, distance, elapsed_time, moving_time, average_speed, max_speed)
-
-
-        }
-        return stravaReturn
-    },
-    testSlackBlock(argument) {
-
-        return {
-            "blocks": [{
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "This is a " + argument + " section block :ghost: *this is bold*, and ~this is crossed out~, and <https://google.com|this is a link>"
-                }
-            }]
-        }
-    }
-
-}
-
+    const stravaReturn = {
+      channel: channel,
+      text: username + " just entered a workout on Strava. ",
+      blocks: blockData(
+        username,
+        distance,
+        elapsed_time,
+        moving_time,
+        average_speed,
+        max_speed
+      ),
+    };
+    return stravaReturn;
+  },
+  testSlackBlock(argument) {
+    return {
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text:
+              "This is a " +
+              argument +
+              " section block :ghost: *this is bold*, and ~this is crossed out~, and <https://google.com|this is a link>",
+          },
+        },
+      ],
+    };
+  },
+};
 
 module.exports = slackTemplates;
